629929031f7f38f0c265635e7b09732f4bc41d76
bin1212 <1147512352@qq.com>
2021-11-21 18:24:19 +0800

    路由监听

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'antd';
 import FormRender, { useForm } from 'form-render';
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
 
 const schema = {
   type: 'object',
@@ -23,15 +24,35 @@ const Demo = () => {
   const form = useForm();
   const onFinish = (formData, errors) => {
     console.log('formData:', formData, 'errors', errors);
-    const a = '123'
-    a.map(item=>{})
+    const a = '123';
+    a.map(item => {});
   };
+  const fn = () => {};
   return (
     <div>
       <FormRender form={form} schema={schema} onFinish={onFinish} />
       <Button type="primary" onClick={form.submit}>
         提交
       </Button>
+      <>
+       
+        {/* <Router>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li> <Link to="/about">About</Link></li>
+          <li> <Link to="/user/1">user1</Link></li>
+          <li> <Link to="/user/2">user2</Link></li>
+        </ul>
+          <Routes>
+            <Route path="/about" element={'about'}></Route>
+            <Route path="/user/:id" element={'about'}></Route>
+            <Route
+              path="/"
+              element={<button onClick={fn}>Break the world</button>}
+            ></Route>
+          </Routes>
+        </Router> */}
+      </>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,22 +3,61 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as Sentry from '@sentry/react'
-import { Integrations } from '@sentry/tracing'
-import {Button } from 'antd'
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+import { Router, Route, Switch, Link, matchPath } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
+const routes = [{ path: '/about' }, { path: '/user/:id' }, { path: '/' }];
 
 Sentry.init({
   dsn:'http://770e5c1447d34453858394602ef4ae7d@118.195.203.216:9000/2',
   environment:'localhost',
-  integrations: [new Integrations.BrowserTracing()],
+  integrations: [new Integrations.BrowserTracing({
+    routingInstrumentation: Sentry.reactRouterV5Instrumentation(history, routes, matchPath),
+  })],
   tracesSampleRate: 1.0,
   release: '0.0.1',
 })
 
 ReactDOM.render(
   <React.StrictMode>
-    <div style={{padding:'20px'}}>
-      <App/>
+    <div style={{ padding: '20px' }}>
+      <Router history={history}>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            {' '}
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            {' '}
+            <Link to="/user/1">user1</Link>
+          </li>
+          <li>
+            {' '}
+            <Link to="/user/2">user2</Link>
+          </li>
+          <li>
+            {' '}
+            <Link to="/app">app</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/about" children={()=><div>about</div>}></Route>
+          <Route path="/app" children={<App />}></Route>
+          <Route path="/user/:id" children={()=><div>user</div>}></Route>
+          <Route
+            path="/"
+            children={()=><button onClick={() => {}}>Break the world</button>}
+          ></Route>
+        </Switch>
+      </Router>
+      {/* <App /> */}
     </div>
   </React.StrictMode>,
   document.getElementById('root')
